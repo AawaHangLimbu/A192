5be234fdc0c6bdc2152fa17c82043d2d149f1a8a
aawahanglimbu <aawahang355@gmail.com>
2025-05-27 07:42:47 +0545

    feat: smething

--- demo25.js
+++ demo25.js
@@ -1,8 +1,25 @@
-const arr=[1,2,3,4,5]
-
-const filterEven =()=>{
-    const result= arr.filter((num)=> num % 2===0
-    )
-    return result
+const UserData=[
+    {id:1, name:"abdul", gender:"male",age:20,occupation:undefined},
+    {id:2, name:"surendra", gender:"male",age:16,occupation:"intern"},
+    {id:3, name:"Kavi", gender:"male",age:21,occupation:"streamer"},
+    {id:4, name:"Sabina", gender:"female",age:18,occupation:undefined},
+    {id:5, name:"rabina", gender:"female",age:19,occupation:"SWE"},
+    {id:6, name:"tripal", gender:"male",age:19,occupation:undefined},
+]
+const response={
+    status:200,
+    message:"user data fetched sucessfylly",
+    data:UserData
 }
-console.log(filterEven());
+const fetchUserNames=()=>{
+    if(response.status === 200){
+        const result=response.data
+        .filter((user)=> user.occupation||user.age >30)
+        .map((user)=> user.name)
+        return result
+    }else{
+        const message="API fetch failed"
+        return message
+    }
+}
+console.log(fetchUserNames())
